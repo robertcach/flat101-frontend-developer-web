4da0b5c77bcfb147e146ad9724a7212c660b6358
Robert Cabrera <cabrera.rch@gmail.com>
2022-06-06 11:11:30 +0200

    Update InputGroup.jsx

--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -4,7 +4,7 @@ const InputGroup = ({ label, id, name, type, placeholder, register }) => {
   return (
     <div className="form-input">
       <label className="form-input__label" htmlFor={id}></label>
-      <input className="form-input__input" type={type} id={id} placeholder={placeholder} {...register(id)}></input>
+      <input className="form-input__input" type={type} id={id} placeholder={placeholder} {...register(id)} required></input>
     </div>
   )
 }
